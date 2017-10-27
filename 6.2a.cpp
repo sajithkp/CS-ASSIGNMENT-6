3a77ebeaf76b732b843b27b59baafdbd3a220a01
sajithkp <31349721+sajithkp@users.noreply.github.com>
2017-10-27 21:58:14 +0530

    Add files via upload

---
+++ 6.2a.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+
+
+int main()
+
+{
+
+    int a,b;
+
+    cout << "Enter a & b respectively : ";
+
+    cin >> a >> b;
+
+    int *p;
+
+    p = &a;
+
+    b = a;
+
+    cout << "a = " << a << "\nb = " << b << "\n*p = " << *p;
+
+}
